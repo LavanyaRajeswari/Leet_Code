011bae36f164d48f1b38138c9097e767180932fc
LavanyaRajeswari <146519974+LavanyaRajeswari@users.noreply.github.com>
2025-02-06 18:24:40 +0530

    Time: 1 ms (92.46%) | Memory: 7.7 MB (93.79%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.cpp
+++ 1332-count-vowels-permutation/count-vowels-permutation.cpp
@@ -1,44 +1,21 @@
 class Solution {
 public:
-    int dp[100001][26];
-    int mod = 1e9+7;
-    long long per(char ch,int i,int n,vector<vector<int>>&dp) {
-        if(i == n) {
-            return 1;
-        } 
-       if(dp[i][ch-'a']!=-1)return dp[i][ch-'a'];
-        long long count = 0;
-        if(ch == 'a') {
-            count += (per('e',i+1,n,dp) % mod);
-        }
-        else if(ch == 'e') {
-            count += (per('a',i+1,n,dp)% mod);
-            count += (per('i',i+1,n,dp)% mod);
-        }
-        else if(ch == 'i') {
-            count += (per('a',i+1,n,dp)% mod);
-            count += (per('e',i+1,n,dp)% mod);
-            count += (per('o',i+1,n,dp)% mod);
-            count += (per('u',i+1,n,dp)% mod);
-        }
-        else if(ch == 'o') {
-            count += (per('i',i+1,n,dp)% mod);
-            count += (per('u',i+1,n,dp)% mod);
-        }
-        else if(ch == 'u') {
-            count += (per('a',i+1,n,dp)% mod);
-        }
-        return dp[i][ch-'a'] = (count % mod);
-    }
     int countVowelPermutation(int n) {
-        long long ans = 0;
-        vector<vector<int>>dp(n+1,vector<int>(26,-1));
-        ans += (per('a',1,n,dp)% mod);
-        ans += (per('e',1,n,dp)% mod);
-        ans += (per('i',1,n,dp)% mod);
-        ans += (per('o',1,n,dp)% mod);
-        ans += (per('u',1,n,dp)% mod);
-        ans %= mod;
-        return ans;
+        int a = 1,e = 1,i = 1,o = 1,u = 1;
+        int na,ne,ni,no,nu;
+        int mod = 1e9+7;
+        while (--n){
+            na = (e%mod);
+            ne = (a+i)%mod;
+            ni = (1ll*a+e+o+u)%mod;
+            no = (i+u)%mod;
+            nu = (a)%mod;
+            a = (na)%mod;
+            e = (ne)%mod;
+            i = (ni)%mod;
+            o = (no)%mod;
+            u = (nu)%mod;
+        }
+        return (1ll*a+e+i+o+u)%mod;
     }
 };
