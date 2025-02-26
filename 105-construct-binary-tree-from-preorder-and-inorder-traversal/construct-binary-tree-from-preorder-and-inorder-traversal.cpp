bccf5f935cacb485e5ae9b445548d98af021f166
LavanyaRajeswari <146519974+LavanyaRajeswari@users.noreply.github.com>
2025-02-26 12:56:09 +0530

    Time: 7 ms (39.39%) | Memory: 27.4 MB (56.56%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,22 +11,23 @@
  */
 class Solution {
 public:
-    TreeNode* build(int i,int j,int a,int b,vector<int>& preorder,map<int,int>&mp){
-        if (i > j) return nullptr;
+    int i = 0;
+    TreeNode* bulid(int a,int b,vector<int>&preorder,map<int,int>&mp) {
+        if(a > b) return nullptr;
         TreeNode* root = new TreeNode(preorder[i]);
         int ind = mp[preorder[i]];
-        int left = ind-a;
-        int right = b-ind;
-        root->left = build(i+1,i+left,a,ind-1,preorder,mp);
-        root->right = build(i+left+1,j,ind+1,b,preorder,mp);
+        i++;
+        root->left = bulid(a,ind-1,preorder,mp);
+        root->right = bulid(ind+1,b,preorder,mp);
         return root;
     }
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int n = preorder.size();
-        map<int,int> mp;
-        for (int i=0; i<n; i++){
+        map<int,int>mp;
+        for(int i = 0; i < n; i++) {
             mp[inorder[i]] = i;
         }
-        return build(0,n-1,0,n-1,preorder,mp);
+        return bulid(0,n-1,preorder,mp);
     }
 };
