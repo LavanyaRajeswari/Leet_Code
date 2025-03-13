1978adeafc444d536fe786cd4a4cc8edd228ff2a
LavanyaRajeswari <146519974+LavanyaRajeswari@users.noreply.github.com>
2025-03-13 22:23:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (76.65%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        map<string,int> mp;
+        int mws = 0;
+        for (string word : wordDict){
+            mp[word]++;
+            int si = word.size();
+            mws = max(mws, si);
+        }
+        vector<int> dp(n+1,0);
+        dp[0] = 1;
+        for (int i=0; i<n; i++){
+            for (int j=i; j<min(n,i+mws); j++){
+                string word = s.substr(i,j-i+1);
+                if (mp.find(word) != mp.end() && dp[i]==1){
+                    dp[j+1] = 1;
+                    // break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
