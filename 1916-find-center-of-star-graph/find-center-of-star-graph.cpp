fc5bd98cd5ad21dbb706cc39898c60162c290fda
LavanyaRajeswari <146519974+LavanyaRajeswari@users.noreply.github.com>
2025-03-19 23:43:56 +0530

    Time: 49 ms (23.42%) | Memory: 124.4 MB (7.97%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int findCenter(vector<vector<int>>& ed) {
         int n = ed.size();
-        vector<int>in(n+2);
-        for(auto i:ed) {
-            in[i[0]]++;
-            in[i[1]]++;
+        vector<vector<int>>adj(n+2);
+        for(int i = 0; i < ed.size(); i++) {
+            adj[ed[i][0]].push_back(ed[i][1]);
+            adj[ed[i][1]].push_back(ed[i][0]);
         }
         for(int i = 1; i <= n+1; i++) {
-            if(in[i] > 1) return i;
+            if(adj[i].size() > 1) return i;
         }
         return 0;
     }
