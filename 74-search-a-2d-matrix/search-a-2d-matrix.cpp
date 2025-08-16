bb38c98e6338e2a7d5162271b3de683ababede70
LavanyaRajeswari <146519974+LavanyaRajeswari@users.noreply.github.com>
2025-08-16 16:37:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.01%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int tar) {
+        int n = mat.size()-1, m = mat[0].size()-1;
+        for(int i = 0; i <= n; i++) {
+            if(tar >= mat[i][0] and tar <= mat[i][m]) {
+                int l = 0, h = m;
+                while(l <= h) {
+                    int m = (l+h)/2;
+                    if(mat[i][m] == tar) return true;
+                    if(mat[i][m] < tar) l = m+1;
+                    else h = m-1;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+};
