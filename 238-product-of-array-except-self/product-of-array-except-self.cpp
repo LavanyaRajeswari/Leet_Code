9c517a35b52a56ec4067a95dab6c620d8ad04adf
Lavanya <146519974+LavanyaRajeswari@users.noreply.github.com>
2026-07-22 12:37:31 +0530

    Time: 4 ms (21.15%) | Memory: 46.8 MB (5.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int z = 0,product = 1;
+        vector<int>pre;
+        vector<int>suf;
         vector<int>ans;
-        for(int i=0;i<nums.size();i++){ 
-        if(nums[i] == 0) z++;
-        if(nums[i]!=0){
-        product*=nums[i];
+        int n = nums.size();
+        int x = 1;
+        for(int i = 0; i < n; i++) {
+            x *= nums[i];
+            pre.push_back(x);
         }
+        x = 1;
+        for(int i = n-1; i >= 0; i--) {
+            x *= nums[i];
+            suf.push_back(x);
         }
-        if(z > 1){
-            for(int i=0;i<nums.size();i++)
-            ans.push_back(0);
-        }
-        else if(z == 1){
-            for(int i=0;i<nums.size();i++)
-            if(nums[i]!=0) ans.push_back(0);
-            else ans.push_back(product);
-        }
-        else{
-            for(int i=0;i<nums.size();i++)
-            ans.push_back(product/nums[i]);
+        reverse(suf.begin(), suf.end());
+        for(int i = 0; i < n; i++) {
+            int st = i - 1, ed = i + 1;
+            if(i == 0) {
+                ans.push_back(suf[ed]);
+            }
+            else if(i == n-1) {
+                ans.push_back(pre[st]);
+            }
+            else {
+                ans.push_back(pre[st] * suf[ed]);
+            }
         }
         return ans;
     }
